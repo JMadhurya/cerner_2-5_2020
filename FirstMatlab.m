fbba44470f4d1285da6a06f1abb6a8325578023a
JMadhurya <71380361+JMadhurya@users.noreply.github.com>
2020-10-15 13:42:07 +0530

    Create FirstMatlab.m

---
+++ FirstMatlab.m
@@ -0,0 +1,9 @@
+% Simple program in matlab to display text cerner_2^5_2020
+classdef hello
+    methods
+        function greet(this)
+            disp('Hi Cerner :)')
+        end
+    end
+end
+%cerner_2^5_2020
