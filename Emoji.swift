f3f6eeca58a998595acdb4e6ec0b5ab6c89a5a32
JMadhurya <71380361+JMadhurya@users.noreply.github.com>
2020-10-16 16:05:56 +0530

    Create Emoji.swift

---
+++ Emoji.swift
@@ -0,0 +1,10 @@
+// cerner_2^5_2020
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Display Emoji's")
+	fmt.Println("\U0001f600 , \U0001F606 , \U0001F923")
+}// My first Swift program to display emoji using unicode  cerner_2^5_2020
+
+
